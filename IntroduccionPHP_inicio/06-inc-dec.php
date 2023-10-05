0fd5f33ddec13724625d79b02204cb1d90506bac
patxi <patxiarana05@gmail.com>
2023-10-05 14:23:53 -0300

    Incremento Decremento

--- IntroduccionPHP_inicio/06-inc-dec.php
+++ IntroduccionPHP_inicio/06-inc-dec.php
@@ -1,5 +1,23 @@
 <?php include 'includes/header.php';
 
+//Incremento 
+$numero1 = 30 ;
+$numero1 ++ ;
+
+echo($numero1) ;
+
+//Incremento con numero 
+$numero1 = 80 ;
+$numero1 =+ 5 ;
+
+echo($numero1) ; 
+
+//DECREMENTO 
+
+$numero2 = 30 ;
+$numero2 -- ;
+
+echo($numero2) ;
 
 
 
