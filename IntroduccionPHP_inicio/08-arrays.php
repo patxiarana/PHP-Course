9e0ea217766aa563a64b94ab87e1819c0ac3d89b
patxi <patxiarana05@gmail.com>
2023-10-05 15:11:28 -0300

    arrays y array asociativos

--- IntroduccionPHP_inicio/08-arrays.php
+++ IntroduccionPHP_inicio/08-arrays.php
@@ -1,6 +1,57 @@
 <?php include 'includes/header.php';
 
+//Estilo de crear Arreglos en laravel 
+$array = ['tablet', 'television', 'computadora'] ;
 
+//Util para ver los contenidos de un array
+echo "<pre>" ; 
+
+var_dump($array) ; 
+
+"<pre>" ; 
+
+//Acceder a un elemento del array 
+echo $array[1] ; 
+
+// Añade un elemento en el indice tres del arreglo
+$array[3] = 'Nuevo Producto' ; 
+
+
+
+
+echo "<pre>" ; 
+
+var_dump($array) ; 
+
+"<pre>" ; 
+
+
+// Añade un elemnto con push al final del arreglo 
+
+array_push($array, 'Audifono') ; 
+
+
+//AñAdir al inicio 
+
+array_unshift($array, 'samartwatch') ; 
+
+
+
+echo "<pre>" ; 
+
+var_dump($array) ; 
+
+"<pre>" ; 
+
+//Estilo de crear Arreglos en wordpresss
+
+$cliente = array('cliente1', 'cliente2', 'cliente3') ;
+
+echo "<pre>" ; 
+
+var_dump($cliente) ; 
+
+"<pre>" ; 
 
 
 include 'includes/footer.php';
--- IntroduccionPHP_inicio/09-arrays-assoc.php
+++ IntroduccionPHP_inicio/09-arrays-assoc.php
@@ -1,6 +1,28 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Patxi',
+    'saldo' => 200 ,
+    'informacion' => [
+        'tipo' => 'premium',
+        'disponible' => 100 , 
+    ]
+] ; 
+
+$cliente['codigo'] = 12315 ; 
 
 
+echo "<pre>" ; 
+
+var_dump($cliente) ; 
+
+"<pre>" ; 
+
+echo $cliente['nombre'] ; 
+echo $cliente['saldo'] ; 
+
+// Acceder al array tipo
+
+echo $cliente['informacion']['tipo']; 
 
 include 'includes/footer.php';
