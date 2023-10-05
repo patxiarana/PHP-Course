eadd56121609ce1ec23a35baab08f876f7f74e35
patxi <patxiarana05@gmail.com>
2023-10-05 14:06:48 -0300

    Tipos de datos en php

--- IntroduccionPHP_inicio/03-tipos-dato.php
+++ IntroduccionPHP_inicio/03-tipos-dato.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+//Bolean 
+
+$logueado = true ; // false --> Valores boleans al igual que en JavaScript ; 
+var_dump(($logueado)) ; //utilizamos dump para ver mas informacion sobre la variable 
 
 
+//Enteros
+$numerosEnteros = 20 ; 
+var_dump($numerosEnteros) ; 
 
+
+//Float 
+$Float = 200.5 ;
+var_dump($Float) ; 
+
+
+//String
+$nombre = "Juan" ;
+var_dump($nombre) ;
+
+//Arreglos 
+$Array = [] ;
+var_dump($Array) ; 
 include 'includes/footer.php';
