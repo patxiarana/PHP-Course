6e3d822ea42f694f836a55d4f4a7193603000e05
patxi <patxiarana05@gmail.com>
2023-10-05 14:12:48 -0300

    operadores

--- IntroduccionPHP_inicio/04-operadores.php
+++ IntroduccionPHP_inicio/04-operadores.php
@@ -1,5 +1,31 @@
 <?php include 'includes/header.php';
 
+$numero1 = 10 ;
+$numero2 = 20 ;
+
+
+//Sumar 
+echo $numero1 + $numero2 ;
+
+//Resta 
+echo $numero1 - $numero2 ;
+
+
+echo "<br>" ; 
+
+//Multiplicar 
+echo   $numero1 * $numero2; 
+
+
+echo "<br>" ; 
+//Dividir 
+
+echo $numero1 / $numero2;
+
+echo "<br>" ; 
+//Multiplicar cierta cantidad de veces 
+echo 2 ** 2 ;
+
 
 
 
