6313284b07bbebda55652b54cbd7d36e82fcddea
patxi <patxiarana05@gmail.com>
2023-10-05 15:21:00 -0300

    isset y empty metodos

--- IntroduccionPHP_inicio/10-isset-empty.php
+++ IntroduccionPHP_inicio/10-isset-empty.php
@@ -1,6 +1,29 @@
 <?php include 'includes/header.php';
 
+$clientes = [];
+$clientes1 = array();
+$clientes2 = array('pedro', 'juan', 'karen') ; 
 
+$cliente = [
+    'nombre' => 'juan',
+    'saldo' => 200
+] ; 
 
+//empty - revisa si un arreglo esta vacio
 
+var_dump(empty($clientes)); 
+var_dump(empty($clientes1)); 
+var_dump(empty($clientes2)); 
+
+echo "<br>" ; 
+//issete  - revisa si una arreglo o una propiedad a sido creada o definida 
+
+var_dump(isset($clientes1)) ; 
+var_dump(isset($noncreate)) ; 
+
+echo "<br>" ; 
+
+//Isset permite revisar si una propiedad de un arreglo asociativo existe 
+var_dump(isset($cliente['nombre'])); //true
+var_dump(isset($cliente['apellido'])); // false 
 include 'includes/footer.php';
