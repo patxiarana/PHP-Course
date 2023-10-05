8082513decfe7859cce34cb785834618b168f2a1
patxi <patxiarana05@gmail.com>
2023-10-05 12:57:37 -0300

    Hello World in PHP

---
+++ IntroduccionPHP_inicio/01-hola-mundo.php
@@ -0,0 +1,14 @@
+
+<?php 
+//echo "Hola mundo" ;
+//echo ('Hola mundo');
+
+//print "Hola Mundo" ;
+
+//print("Hola Mundo");
+
+//print_r("Hola Mundo");
+
+//var_dump("Hola mundo") ;
+
+var_dump(20) ;
