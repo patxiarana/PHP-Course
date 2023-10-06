9ada69c4917f8d7f30e19714720211ddec36cfd8
patxi <patxiarana05@gmail.com>
2023-10-06 15:39:19 -0300

    funciones

--- IntroduccionPHP_inicio/15-funciones.php
+++ IntroduccionPHP_inicio/15-funciones.php
@@ -1,6 +1,16 @@
 <?php include 'includes/header.php';
 
+function sumar( int $numero1 = 0  , int $numero2 = 0 ) {
+    echo $numero1 + $numero2 ; 
+}
 
-
+sumar(5,5);
+echo "<br>";
+sumar(40,5);
+echo "<br>";
+sumar();
+echo "<br>";
+sumar(25,30);
+echo "<br>";
 
 include 'includes/footer.php';
