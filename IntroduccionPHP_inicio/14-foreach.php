fe53a7849ebdaa9b54a995f92c7a573e430d28da
patxi <patxiarana05@gmail.com>
2023-10-06 15:31:18 -0300

    foreach mostrando datos

--- IntroduccionPHP_inicio/14-foreach.php
+++ IntroduccionPHP_inicio/14-foreach.php
@@ -1,5 +1,5 @@
 <?php include 'includes/header.php';
-
+/*
 $cliente  = array('Pedro', 'Juan', 'Karen') ; 
 
 
@@ -17,7 +17,48 @@ $cliente = [
 
 foreach($cliente as $key => $valor) {
     echo  $key. "-" .$valor ."<br>" ; 
-} ; 
+} ; */
+
+$productos = array(
+    array(
+        'nombre' => 'Producto 1',
+        'precio' => rand(10, 50), // Genera un precio aleatorio entre 10 y 50
+        'disponibilidad' => true // Genera un valor booleano aleatorio (0 o 1)
+    ),
+    array(
+        'nombre' => 'Producto 2',
+        'precio' => rand(10, 50),
+        'disponibilidad' => false
+    ),
+    array(
+        'nombre' => 'Producto 3',
+        'precio' => rand(10, 50),
+        'disponibilidad' => true
+    )
+);
+
+foreach($productos as $producto) { ?>
+    <li>
+    <p>Producto : <?php echo $producto['nombre'] ; ?> </p>
+    <p> Precio  :  <?php echo $producto['precio']  ; ?> </p>
+    <p> <?php echo ($producto['disponibilidad'] ? 'Disponible' : 'no disponible'); ?>
+    <?php /*
+    if($producto['disponibilidad']) {
+        echo  '<p>Disponible<p>' ; 
+    } else {
+        echo  '<p>No Disponible<p>' ; 
+    } ; 
+    ?>
+    </li>
+    */ 
+  
+  echo "<pre>";
+  var_dump($producto);
+  echo  "<pre>" ; 
+
+}
+
+
 
 
 
--- IntroduccionPHP_inicio/15-funciones.php
+++ IntroduccionPHP_inicio/15-funciones.php
@@ -3,5 +3,4 @@
 
 
 
-
 include 'includes/footer.php';
