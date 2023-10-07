2c9b6f817597efe8210188a4fdcb14fc52cdd773
patxi <patxiarana05@gmail.com>
2023-10-07 07:09:29 -0300

    types in php

--- IntroduccionPHP_inicio/16-types.php
+++ IntroduccionPHP_inicio/16-types.php
@@ -1,7 +1,19 @@
 <?php include 'includes/header.php';
 
 
+declare(stritc_types = 1) ; 
+
+function usuarioAutenticado(bool $autenticado) : string | int {
+   if($autenticado) {
+    return  "El usuario esta autenticad" ; 
+   } else {
+    return 20 ; 
+   }
+} ; 
 
 
+$usuario = usuarioAutenticado(true) ; 
+
+echo $usuario ; 
 
 include 'includes/footer.php';
